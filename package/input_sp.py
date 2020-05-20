4343cb1c918929b476ca1eed52857000a8a094ec
Zhili Wen <wenzhili523@gmail.com>
2020-05-20 17:40:02 +0000

    Remove file generation function, update the test file

--- package/input_sp.py
+++ package/input_sp.py
@@ -16,15 +16,3 @@ class input_sp(inputfile):
     def __init__(self):
         inputfile.__init__(self)
     
-    def generate_sp_inputfile(self):
-        """Function to generate the inputfile for single-point calculation.
-        
-        Args: 
-            None
-        
-        Returns: 
-            com file: inputfile for calculation
-    
-        """
-        f = open("{}.com".format(self.name),'w')
-        f.write("$RunGauss\n%NprocLinda=1\n%Mem={}gb\n%NProcshared={}\n# {}\n\nTest\n\n{},{}\n{}".format(self.memory, self.cpu, self.level, self.charge, self.multiplicity, self.coordinate))
--- package/input_sp_test.py
+++ package/input_sp_test.py
@@ -3,4 +3,4 @@ from input_sp import input_sp
 sp_one = input_sp()
 sp_one.input_condition()
 sp_one.input_coordinate()
-sp_one.generate_sp_inputfile()
+sp_one.generate_inputfile()
