ab15594b81eb737973a5d631836946472708f963
Zhili Wen <wenzhili523@gmail.com>
2020-05-20 21:01:53 +0000

    Update to package format

--- example/input_opt_test.py
+++
@@ -1,7 +0,0 @@
-from Gaussian_inputfile import input_opt
-
-opt_one = input_opt()
-opt_one.input_condition()
-opt_one.input_coordinate()
-opt_one.opt_freq()
-opt_one.generate_inputfile()
--- example/input_sp_test.py
+++
@@ -1,6 +0,0 @@
-from Gaussian_inputfile import input_sp
-
-sp_one = input_sp()
-sp_one.input_condition()
-sp_one.input_coordinate()
-sp_one.generate_inputfile()
