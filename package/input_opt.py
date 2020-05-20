863730144a06a7d795e81dc15f8143c1c2de2bd7
Zhili Wen <wenzhili523@gmail.com>
2020-05-20 17:41:13 +0000

    Update input_opt class and its testing file

---
+++ package/input_opt.py
@@ -0,0 +1,34 @@
+from inputfile_creation import inputfile
+class input_opt(inputfile):
+    """ Generating inputfiles for geometry optimization calculations. 
+    
+        Attributes:
+            charge (float) define the charge of system
+            multiplicity (float) define the multiplicity of system
+            memory (string) requested memory for calculation
+            cpu (float) requested cpu for calculation
+            funtion (string) defined function for calculation
+            basis_set (string) defined basis_set for calculation
+            dispersion (string) enable the dispersion or not
+            name (string) defined the name of inputfile
+            coordinate (list or string) a list or string of molecule coordinates
+    """
+    def __init__(self):
+        inputfile.__init__(self)
+    
+    def opt_freq(self):
+        """Function to define the optimization condition.
+        
+        Args: 
+            Input scf condition from user
+        
+        Returns: 
+            None
+        """
+        
+        scf=input("\ndefine your scf procedure (type none to set up defaut scf=tight): ")
+        if scf=="none":
+            self.level = self.level + " opt freq"
+        else:
+            self.level = self.level + " opt freq " + scf
+    
---
+++ package/input_opt_test.py
@@ -0,0 +1,7 @@
+from input_opt import input_opt
+
+opt_one = input_opt()
+opt_one.input_condition()
+opt_one.input_coordinate()
+opt_one.opt_freq()
+opt_one.generate_inputfile()
